fdd0e60a7e7c32f0322986acd92ce0fbc2f59332
Briana Wright <brianaleew@gmail.com>
2023-03-09 16:42:49 -0500

    edit and delete now working on apparel items

--- stylelist/main_app/urls.py
+++ stylelist/main_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('apparels/', views.apparels_index, name='apparels_index'),
     path('apparels/<int:apparel_id>/', views.apparels_detail, name='detail'),
     path('apparels/create/', views.ApparelCreate.as_view(), name='apparels_create'),
+    path('apparels/<int:pk>/update/', views.ApparelUpdate.as_view(), name='apparels_update'),
     path('apparels/<int:pk>/delete/', views.ApparelDelete.as_view(), name='apparels_delete'),
     # Outfit urls 
     path('outfits/', views.outfits_index, name='outfits_index'),
